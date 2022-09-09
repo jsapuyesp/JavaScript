84fcd8bdf03aba79112f05d3534d9bdbfd493b66
jsapuyesp <jsapuyesp@gmail.com>
2022-09-08 20:11:35 -0500

    Inicios de la seccion 2

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,17 @@
+
+let nombre = 'Peter';
+console.log(nombre);
+
+nombre = 'Ben';
+console.log(`Actualizado: ${nombre}`);
+
+console.log(typeof nombre);
+// ---
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+let Numero = 123;
+console.log(typeof Numero);
+
+let superPoder;
+console.log(typeof superPoder);
